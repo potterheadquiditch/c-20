48ca5c531f13358aa3f38890789f6b76c833f473
potterheadquiditch <74307161+potterheadquiditch@users.noreply.github.com>
2020-11-19 19:52:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedrect,movingrect
+
+
+function setup() {
+  createCanvas(1200,800);
+ fixedrect= createSprite(600, 400, 50, 80);
+ fixedrect.shapeColor="red"
+ fixedrect.debug=true
+ movingrect= createSprite(400, 200, 80, 30);
+ movingrect.shapeColor="red"
+ movingrect.debug=true
+
+}
+
+function draw() {
+  background(255,255,255);  
+  movingrect.y=World.mouseY
+  movingrect.x=World.mouseX
+if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2&&
+  fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2&&
+  movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2&&
+  fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2){
+  movingrect.shapeColor="green";
+  fixedrect .shapeColor="green";
+}
+else{
+  movingrect.shapeColor="red";
+  fixedrect .shapeColor="red";
+}
+  drawSprites();
+}
